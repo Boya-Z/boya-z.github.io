4ae1b408c9d95b1dcb262f936016b382aa5c1ed6
Boya-Z <55926145+Boya-Z@users.noreply.github.com>
2024-05-23 14:29:57 -0500

    course update

--- src/pages/courses/Courses.js
+++ src/pages/courses/Courses.js
@@ -5,6 +5,7 @@ import '../../style.css';
 import { AiFillStar } from "react-icons/ai";
 import LoadingGif from "../../assets/img/loading.gif";
 import ads from "../../assets/img/ads.png";
+import uwcs from "../../assets/img/uwcs.png";
 
 function Course_Taken() {
     return (
@@ -28,9 +29,9 @@ function Course_Taken() {
                         <ul className=" portfolio_right_text list">
                             <strong>Big Data and Cloud Computing </strong> <br />
                             <strong>Professor:</strong> <br />
-                            <strong>Rating: <AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/> </strong> <br />
+                           
                             <strong>When: Autumn 2023 </strong> <br />
-                            <strong>Workload: <AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/> </strong>
+                            <strong>Workload: <AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/><AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/>  </strong>
                         </ul>
                     </div>
 
@@ -39,7 +40,7 @@ function Course_Taken() {
                         <ul className=" portfolio_right_text list">
                             <strong>Leadership & Consulting in Data Science</strong> <br />
                             <strong>Professor:</strong> <br />
-                            <strong>Rating: <AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/> </strong> <br />
+
                             <strong>When: Autumn 2023 </strong> <br />
                             <strong>Workload: <AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/> </strong>
                         </ul>
@@ -50,7 +51,7 @@ function Course_Taken() {
                         <ul className=" portfolio_right_text list">
                             <strong>Statistical Analysis</strong> <br />
                             <strong>Professor:</strong> <br />
-                            <strong>Rating: <AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/> </strong> <br />
+                            
                             <strong>When: Autumn 2023 </strong> <br />
                             <strong>Workload: <AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/> </strong>
                         </ul>
@@ -61,9 +62,9 @@ function Course_Taken() {
                         <ul className=" portfolio_right_text list">
                             <strong>Natural Language Processing and Cognitive Computing</strong> <br />
                             <strong>Professor:</strong> <br />
-                            <strong>Rating: <AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/> </strong> <br />
+                            
                             <strong>When: Winter 2024 </strong> <br />
-                            <strong>Workload: <AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/> </strong>
+                            <strong>Workload: <AiFillStar color="#FFDB58"/><AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/>  <AiFillStar color="#FFDB58"/> </strong>
                         </ul>
                     </div>
                     <div className="d-flex mt-4 ms-5">
@@ -71,9 +72,9 @@ function Course_Taken() {
                         <ul className=" portfolio_right_text list">
                             <strong>Data Mining Principles</strong> <br />
                             <strong>Professor:</strong> <br />
-                            <strong>Rating: <AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/> </strong> <br />
+                            
                             <strong>When: Winter 2024 </strong> <br />
-                            <strong>Workload: <AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/> </strong>
+                            <strong>Workload: <AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/> </strong>
                         </ul>
                     </div>
                     <div className="d-flex mt-4 ms-5">
@@ -81,7 +82,7 @@ function Course_Taken() {
                         <ul className=" portfolio_right_text list">
                             <strong>Linear and Non-Linear Models</strong> <br />
                             <strong>Professor:</strong> <br />
-                            <strong>Rating: <AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/> </strong> <br />
+                           
                             <strong>When: Winter 2024 </strong> <br />
                             <strong>Workload: <AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/> </strong>
                         </ul>
@@ -91,9 +92,9 @@ function Course_Taken() {
                         <ul className=" portfolio_right_text list">
                             <strong>Machine Learning & Predictive Analytics</strong> <br />
                             <strong>Professor:</strong> <br />
-                            <strong>Rating: <AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/> </strong> <br />
+                            
                             <strong>When: Spring 2024 </strong> <br />
-                            <strong>Workload: <AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/> </strong>
+                            <strong>Workload: <AiFillStar color="#FFDB58"/><AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/> </strong>
                         </ul>
                     </div>
                     <div className="d-flex mt-4 ms-5">
@@ -101,9 +102,9 @@ function Course_Taken() {
                         <ul className=" portfolio_right_text list">
                             <strong>Time Series Analysis and Forecasting</strong> <br />
                             <strong>Professor:</strong> <br />
-                            <strong>Rating: <AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/> </strong> <br />
+                            
                             <strong>When: Spring 2024 </strong> <br />
-                            <strong>Workload: <AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/> </strong>
+                            <strong>Workload: <AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/> <AiFillStar color="#FFDB58"/> </strong>
                         </ul>
                     </div>
 
@@ -112,7 +113,7 @@ function Course_Taken() {
 
                     <h4 className="text-center">Course taken at University of Wisconsin - Madison</h4> <br /> <br />
                     <div className="d-flex mt-4 ms-5">
-                        <img src={LoadingGif} alt="cs200" width={250} height={250}/>
+                        <img src={uwcs} alt="cs200" width={250} height={100}/>
                         <ul className=" portfolio_right_text list">
                             <strong>COMP SCI 200 - Programming I </strong> <br />
                             <strong>Professor: </strong> <br />
@@ -123,7 +124,7 @@ function Course_Taken() {
                     </div>
 
                     <div className="d-flex mt-4 ms-5">
-                    <img src={LoadingGif} alt="cs220" width={250} height={250}/>
+                    <img src={uwcs} alt="cs220" width={250} height={100}/>
                     <ul className=" portfolio_right_text list">
                         <strong>COMP SCI 220 - Data Science Programming I </strong> <br />
                         <strong>Professor: </strong> <br />
@@ -134,7 +135,7 @@ function Course_Taken() {
                     </div>
 
                     <div className="d-flex mt-4 ms-5">
-                    <img src={LoadingGif} alt="cs240" width={250} height={250}/>
+                    <img src={uwcs} alt="cs240" width={250} height={100}/>
                     <ul className=" portfolio_right_text list">
                         <strong>COMP SCI 240 - Introduction to Discrete Mathematics </strong> <br />
                         <strong>Professor: </strong> <br />
@@ -145,7 +146,7 @@ function Course_Taken() {
                     </div>
 
                     <div className="d-flex mt-4 ms-5">
-                    <img src={LoadingGif} alt="cs252" width={250} height={250}/>
+                    <img src={uwcs} alt="cs252" width={250} height={100}/>
                     <ul className=" portfolio_right_text list">
                         <strong>COMP SCI 252 - Introduction to Computer Engineering </strong> <br />
                         <strong>Professor: </strong> <br />
@@ -156,7 +157,7 @@ function Course_Taken() {
                     </div>
 
                     <div className="d-flex mt-4 ms-5">
-                    <img src={LoadingGif} alt="cs300" width={250} height={250}/>
+                    <img src={uwcs} alt="cs300" width={250} height={100}/>
                     <ul className=" portfolio_right_text list">
                         <strong>COMP SCI 300 - Programming II </strong> <br />
                         <strong>Professor: </strong> <br />
@@ -167,7 +168,7 @@ function Course_Taken() {
                     </div>
 
                     <div className="d-flex mt-4 ms-5">
-                    <img src={LoadingGif} alt="cs320" width={250} height={250}/>
+                    <img src={uwcs} alt="cs320" width={250} height={100}/>
                     <ul className=" portfolio_right_text list">
                         <strong>COMP SCI 320 - Data Science Programming II </strong> <br />
                         <strong>Professor: </strong> <br />
@@ -178,7 +179,7 @@ function Course_Taken() {
                     </div>
 
                     <div className="d-flex mt-4 ms-5">
-                    <img src={LoadingGif} alt="cs354" width={250} height={250}/>
+                    <img src={LoadingGif} alt="cs354" width={250} height={100}/>
                     <ul className=" portfolio_right_text list">
                         <strong>COMP SCI 354 - Machine Organization and Programming </strong> <br />
                         <strong>Professor: </strong> <br />
@@ -189,7 +190,7 @@ function Course_Taken() {
                     </div>
 
                     <div className="d-flex mt-4 ms-5">
-                    <img src={LoadingGif} alt="cs400" width={250} height={250}/>
+                    <img src={uwcs} alt="cs400" width={250} height={100}/>
                     <ul className=" portfolio_right_text list">
                         <strong>COMP SCI 400 - Programming III </strong> <br />
                         <strong>Professor: </strong> <br />
@@ -200,7 +201,7 @@ function Course_Taken() {
                     </div>
 
                     <div className="d-flex mt-4 ms-5">
-                    <img src={LoadingGif} alt="cs532" width={250} height={250}/>
+                    <img src={uwcs} alt="cs532" width={250} height={250}/>
                     <ul className=" portfolio_right_text list">
                         <strong>COMP SCI 532 - Matrix Methods in Machine Learning</strong> <br />
                         <strong>Professor: </strong> <br />
@@ -211,7 +212,7 @@ function Course_Taken() {
                     </div>
 
                     <div className="d-flex mt-4 ms-5">
-                    <img src={LoadingGif} alt="cs537" width={250} height={250}/>
+                    <img src={uwcs} alt="cs537" width={250} height={100}/>
                     <ul className=" portfolio_right_text list">
                         <strong>COMP SCI 537 - Introduction to Operating Systems</strong> <br />
                         <strong>Professor: </strong> <br />
@@ -222,7 +223,7 @@ function Course_Taken() {
                     </div>
 
                     <div className="d-flex mt-4 ms-5">
-                    <img src={LoadingGif} alt="cs540" width={250} height={250}/>
+                    <img src={uwcs} alt="cs540" width={250} height={100}/>
                     <ul className=" portfolio_right_text list">
                         <strong>COMP SCI 540 - Introduction to Artificial Intelligence</strong> <br />
                         <strong>Professor: </strong> <br />
@@ -233,7 +234,7 @@ function Course_Taken() {
                     </div>
 
                     <div className="d-flex mt-4 ms-5">
-                    <img src={LoadingGif} alt="cs559" width={250} height={250}/>
+                    <img src={uwcs} alt="cs559" width={250} height={100}/>
                     <ul className=" portfolio_right_text list">
                         <strong>COMP SCI 559 - Computer Graphics</strong> <br />
                         <strong>Professor: </strong> <br />
@@ -244,7 +245,7 @@ function Course_Taken() {
                     </div>
 
                     <div className="d-flex mt-4 ms-5">
-                    <img src={LoadingGif} alt="cs564" width={250} height={250}/>
+                    <img src={uwcs} alt="cs564" width={250} height={100}/>
                     <ul className=" portfolio_right_text list">
                         <strong>COMP SCI 564 - Database Management Systems: Design and Implementation</strong> <br />
                         <strong>Professor: </strong> <br />
@@ -255,7 +256,7 @@ function Course_Taken() {
                     </div>
 
                     <div className="d-flex mt-4 ms-5">
-                    <img src={LoadingGif} alt="cs564" width={250} height={250}/>
+                    <img src={uwcs} alt="cs564" width={250} height={100}/>
                     <ul className=" portfolio_right_text list">
                         <strong>COMP SCI 577 - Introduction to Algorithms</strong> <br />
                         <strong>Professor: </strong> <br />
