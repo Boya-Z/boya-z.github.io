b2237247073b9df9c13b5f09132dc7e3d348d376
Boya-Z <55926145+Boya-Z@users.noreply.github.com>
2024-05-23 16:02:35 -0500

    Update Resume.js

--- src/pages/resume/Resume.js
+++ src/pages/resume/Resume.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Resume from "../../assets/resume/resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
