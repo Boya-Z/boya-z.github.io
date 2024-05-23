ebdf25998ab203f3948a41d05621249e23c5ab82
Boya-Z <55926145+Boya-Z@users.noreply.github.com>
2024-05-23 02:49:02 -0500

    update

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -26,7 +26,7 @@ function Projects() {
                 imgPath={LoadingGif}
                 isBlog={false}
                 title="Time Series Demand Foresting for Divvy Bike Usage in Chicago"
-                description="A forecasting system developed using Python and machine learning models like SARIMA, Prophet, Orbit, and LSTM, optimizing Divvy bike availability and reducing costs. "
+                description="A forecasting system developed using Python and machine learning models like SARIMA, Prophet, Orbit, and LSTM, optimizing Divvy bike availability and reducing costs.                                                                         "
                 ghLink=""
                 //demoLink=""
               />
@@ -36,7 +36,7 @@ function Projects() {
                 imgPath={LoadingGif}
                 isBlog={false}
                 title="Credit Card Fraud Detection System using Deep Learning"
-                description="A fraud detection system developed using deep learning, integrating techniques like Multilayer Neural Networks and CNNs, achieving 97.50% accuracy and 100% precision in identifying fraudulent transactions."
+                description="A fraud detection system developed using deep learning, integrating techniques like Multilayer Neural Networks and CNNs, achieving 97.50% accuracy and 100% precision in identifying fraudulent transactions.                                  "
                 ghLink="https://github.com/Boya-Z/Fraud-Detectoion"
                 //demoLink=""
               />
@@ -56,7 +56,7 @@ function Projects() {
                 imgPath={LoadingGif}
                 isBlog={false}
                 title="Amazon Review Big Data Analysis"
-                description="A analysis of 200k+ Amazon reviews using AWS Glue and Amazon Redshift, with Spark NLP for sentiment analysis, Apache Spark's ALS in SageMaker for personalized recommendations, and K-Means clustering for targeted customer segmentation."
+                description="A analysis of 200k+ Amazon reviews using AWS Glue and Amazon Redshift, with Spark NLP for sentiment analysis, Apache Spark's ALS in SageMaker for personalized recommendations, and K-Means clustering for targeted customer segmentation.      "
                 ghLink=""
                 //demoLink=""
               />
@@ -66,7 +66,7 @@ function Projects() {
                 imgPath={LoadingGif}
                 isBlog={false}
                 title="APAC Region Campaigns KPI Real-Time Interactive Dashboard"
-                description="A dynamic data pipeline using AWS Redshift and Vertica, integrating 10M+ rows with regular updates to Tableau, enhancing decision-making efficiency by 30% with real-time KPI and metric tracking."
+                description="A dynamic data pipeline using AWS Redshift and Vertica, integrating 10M+ rows with regular updates to Tableau, enhancing decision-making efficiency by 30% with real-time KPI and metric tracking.                                              "
                 ghLink=""
                 //demoLink=""
               />
@@ -76,7 +76,7 @@ function Projects() {
                 imgPath={LoadingGif}
                 isBlog={false}
                 title="Computer Vision Project: Automated Image Classification System"
-                description="An advanced image classifier developed using TensorFlow and Keras, applying data augmentation to over 50,000 images and employing CNN architectures like ResNet and VGG, achieving 92% accuracy.e"
+                description="An advanced image classifier developed using TensorFlow and Keras, applying data augmentation to over 50,000 images and employing CNN architectures like ResNet and VGG, achieving 92% accuracy.                                                "
                 ghLink=""
                 //demoLink=""
               />
